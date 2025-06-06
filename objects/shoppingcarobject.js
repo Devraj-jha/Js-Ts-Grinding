94246f2e8c67b1b8b9f35a9cb6b2dd8b6dc7545c
Devraj-jha <devrajjha100806@gmail.com>
2025-06-06 23:49:39 +0530

    ermer

---
+++ objects/shoppingcarobject.js
@@ -0,0 +1,14 @@
+let shoppingCart = {
+  items: [],
+  total: 0,
+
+addItem: function(name,price, quantity = 1){
+  let item = {
+      name: name,
+            price: price,
+            quantity: quantity,
+            subtotal: price * quantity
+  }
+}
+}
+this.items.push(item);
