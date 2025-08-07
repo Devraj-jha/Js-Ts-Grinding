ef52a967e83a75269b7a6716180045b6963cbb73
Devraj-jha <devrajjha100806@gmail.com>
2025-08-07 11:40:02 +0530

    Arrow functions and their differences

---
+++ Javascript/Functions/Arrow_function_and_thier_differences.js
@@ -0,0 +1,26 @@
+
+// arrow function
+
+const add = (a,b) => {
+    return a + b;
+}
+//same output cleaner syntax
+
+const square = n => n * n;
+
+const hello = () => console.log("Hello");
+
+const double = x => x * 2;
+console.log(square(3))
+
+//  When to Use Arrow Functions
+// ✅ Simple, inline, quick functions
+// ✅ Callbacks (map, filter, forEach, etc.)
+// ✅ When you want this from the outer scope
+// ❌ Don’t use for:
+
+// Object methods
+
+// Constructors (new)
+
+// Needing arguments
