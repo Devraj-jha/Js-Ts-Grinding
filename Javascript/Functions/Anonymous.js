b167f92f5eb3600fe255f618a58b34a1945feae3
Devraj-jha <devrajjha100806@gmail.com>
2025-08-07 11:30:53 +0530

    Anonymous functions

---
+++ Javascript/Functions/Anonymous.js
@@ -0,0 +1,18 @@
+//a function with name is anon function
+let greet = function(){
+    console.log("hello");
+}
+
+// these funciton can be used once and throw. away
+
+setTimeout(function(){
+    console.log("yoou.... ")
+},1000) // we used the anon function only once
+
+
+// Why Use Anonymous Functions?
+// ✅ Short
+// ✅ Reusable only if assigned
+// ✅ Great for inline, one-time-use
+// ✅ Less clutte
+
