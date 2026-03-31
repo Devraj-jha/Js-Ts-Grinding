b1fce2af6b5ba0e80b6b111804d3569db94709a4
Devraj-jha <devrajjha100806@gmail.com>
2026-03-31 11:20:16 +0530

    function basics

---
+++ javascript_typescript/Javascript/Functions/FunBasics/FuncBasics.js
@@ -0,0 +1,21 @@
+function name(){
+    console.log(" Hello world")
+    
+}
+name() // function calling. 
+
+function greet(name){
+    console.log("hello" + name)
+
+}
+greet("dj"); // fn parameters.. 
+// inputs passed on function
+
+// function with return value.
+
+function add(a,b){
+    return a + b; 
+
+}
+let result = add(3,3)
+console.log(result)
