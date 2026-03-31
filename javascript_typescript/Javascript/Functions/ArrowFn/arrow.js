d8871254c6c8ac872c055db5a034b9e58246c98f
Devraj-jha <devrajjha100806@gmail.com>
2026-03-31 12:51:12 +0530

    fn

---
+++ javascript_typescript/Javascript/Functions/ArrowFn/arrow.js
@@ -0,0 +1,14 @@
+const fn = () => {
+
+}
+// arrow functions are shorter way to write functions
+
+// no parameter fun.
+const hi = () => console.log();
+
+hi()
+// one parameter
+
+const square = x => x * x; 
+// multiple
+const add = (a,b) => a+ b; 
---
+++ javascript_typescript/Javascript/Functions/FunBasics/Types_of_fun.js
@@ -0,0 +1,14 @@
+// regular function
+function sayHI(){
+    console.log("hi")
+
+}
+// function expression
+let add = function(a,b){
+    return a + b; 
+
+}
+console.log(add(2,3))
+//arrow function
+
+let sub = (a,b) => a - b; 
