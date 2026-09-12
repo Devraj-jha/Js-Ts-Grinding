93469155e01f5c5c102812278c4e0252beea225a
Devraj-jha <devrajjha100806@gmail.com>
2026-09-12 12:50:01 +0530

    function

---
+++ javascript_typescript/Javascript/Functions/Advance function /function_express.js
@@ -0,0 +1,13 @@
+
+const multiple = function(a,b){
+    return a  * b; 
+}
+
+console.log(multiple(2,4))
+
+const hi = () => "hello";
+function greeting(hi){
+    console.log(hi());
+}
+
+greeting(hi)
--- javascript_typescript/Javascript/Functions/Advance function /function_returning_anotherFn.js
+++ javascript_typescript/Javascript/Functions/Advance function /function_returning_anotherFn.js
@@ -8,3 +8,12 @@ const double = multiplier(2);
 
 console.log(double(3)) 
 
+function sayhi(){
+    return "hello";
+}
+
+let hi = sayhi
+
+let l = hi()
+
+console.log(l)
